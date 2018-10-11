2eee82c06da8ca7a7bb7222317cda548bcf6d196
somereason <somereason@gmail.com>
2018-10-11 13:44:58 +0800

    添加对网易严选的支持

--- history_price.js
+++ history_price.js
@@ -2,7 +2,7 @@
 // @name 查看历史价格
 // @namespace win.somereason.web.utils
 // @version 2018.10.10
-// @description 购物网站商品页的商品名称后显示[历史价格按钮],点击后打开"慢慢买"网站,查看历史价格.目前支持京东,淘宝,天猫,亚马逊,当当,苏宁,一号店
+// @description 购物网站商品页的商品名称后显示[历史价格按钮],点击后打开"慢慢买"网站,查看历史价格.目前支持京东,淘宝,天猫,亚马逊,当当,苏宁,网易严选
 // @author somereason
 // @license MIT
 // @date 2018-09-06
@@ -13,6 +13,7 @@
 // @match *://product.dangdang.com/*.html*
 // @match *://product.suning.com/*/*.html*
 // @match *://item.yhd.com/*.html*
+// @match *://you.163.com/item/detail*
 // @grant none
 // ==/UserScript==
 // 
@@ -33,8 +34,16 @@
         titleElement = document.querySelector(".proinfo-title");
     } else if (window.location.href.indexOf("yhd.com") > -1) {
         titleElement = document.querySelector("#detail_sku_main");
+    } else if (window.location.href.indexOf("you.163.com") > -1) {
+        //由于网易严选用的是react,页面初始化的时候元素没有渲染,找不到.所以设置延时1秒,然后在找到 title并添加按钮.
+        setTimeout(function () {
+            setButton(document.querySelector(".intro .name"));
+        }, 1000);
     }
     if (titleElement !== null) {
+        setButton(titleElement);
+    }
+    function setButton(titleElement) {
         titleElement.innerHTML += '<button id="openHistoryPricePage">历史价格</button>';
         document.getElementById("openHistoryPricePage").style.cssText = "width: 110px;height: 32px;background:linear-gradient(#00b79e, #002bffcc);border: 1px solid white;cursor: pointer;color: white;";
         document.getElementById("openHistoryPricePage").onclick = openSite;
